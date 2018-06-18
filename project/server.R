027a7b0d2d848798028a69467619167a673ae14e
Peter Hoffmann <peter@p-h.im>
2018-06-18 14:29:13 +0200

    compare kurtosis with 3 (='excess')

--- project/server.R
+++ project/server.R
@@ -19,23 +19,23 @@ location <- function(input, output, dataset) {
   output$min <- renderText({
     min(dataset()[[input$col]])
   })
-  
+
   output$max <- renderText({
     max(dataset()[[input$col]])
   })
-  
+
   output$median <- renderText({
     median(dataset()[[input$col]])
   })
-  
+
   output$mean <- renderText({
     mean(dataset()[[input$col]])
   })
-  
+
   output$mode <- renderText({
     mode(dataset()[[input$col]])
   })
-  
+
   output$midrange <- renderText({
     midrange(dataset()[[input$col]])
   })
@@ -45,35 +45,35 @@ variation <- function(input, output, dataset) {
   output$range <- renderText({
     range(dataset()[[input$col]])
   })
-  
+
   output$standardDeviation <- renderText({
     sd(dataset()[[input$col]])
   })
-  
+
   output$madMean <- renderText({
     mad(dataset()[[input$col]], center = mean(dataset()[[input$col]]))
   })
-  
+
   output$madMedian <- renderText({
     mad(dataset()[[input$col]], center = median(dataset()[[input$col]]))
   })
-  
-  
+
+
   output$medmed <- renderText({
     medmed(dataset()[[input$col]])
   })
-  
+
   output$coefficientOfVariance <- renderText({
     coefficientOfVariance(dataset()[[input$col]])
   })
-  
+
 }
 
 distribution <- function(input, output, dataset) {
   output$skewness <- renderText({
     skewness(dataset()[[input$col]])
   })
-  
+
   output$skewnessDescription <- renderText({
     skew <- skewness(dataset()[[input$col]])
     if (skew < 0) {
@@ -85,22 +85,23 @@ distribution <- function(input, output, dataset) {
     else{
       "Skew = 0. Die verteilung ist perfekt symmetrisch."
     }
-    
+
   })
-  
+
   output$peakedness <- renderText({
     kurtosis(dataset()[[input$col]])
   })
-  
+
   output$peakednessDescription <- renderText({
     peakedness <- kurtosis(dataset()[[input$col]])
-    if (peakedness < 0) {
-      "Negative kurtosis, Flachgipfelig : Die Daten haben wahrscheinlich leichte Ränder."
+    excess <- peakedness - 3
+    if (excess < 0) {
+      "Negativer Exzess, Flachgipfelig: Die Daten haben wahrscheinlich leichte Ränder."
     } else if (peakedness > 0) {
-      "Positive kurtosis, Steilgipfelig: Die Daten haben wahrscheinlich schwere Ränder."
+      "Positiver Exzess, Steilgipfelig: Die Daten haben wahrscheinlich schwere Ränder."
     } else {
-      "Kurtosis = 0: Eine perfekte Normalverteilung."
-    } 
+      "Exzess = 0 (Kurtosis = 3): Eine perfekte Normalverteilung."
+    }
   })
 }
 
@@ -113,7 +114,7 @@ showPlots <- function(input, output, dataset) {
          col = "powderblue",
          breaks = bins)
   })
-  
+
   output$boxPlot <- renderPlot({
     boxplot(
       dataset()[[input$col]],
@@ -124,7 +125,7 @@ showPlots <- function(input, output, dataset) {
       col = "powderblue"
     )
   })
-  
+
   output$violinPlot <- renderPlot({
     vioplot(
       dataset()[[input$col]],
@@ -134,31 +135,31 @@ showPlots <- function(input, output, dataset) {
     )
     title("Violin Plot")
   })
-  
+
   output$kernelDensityPlot <- renderPlot({
     plot(density(dataset()[[input$col]]),
          main = "Kernel Density Plot",
          xlab = input$col)
   })
-  
+
   output$quantileQuantilePlot <- renderPlot({
     qqnorm(dataset()[[input$col]], main = "Normal Q-Q Plot", xlab = input$col)
     qqline(dataset()[[input$col]], distribution = qnorm)
   })
-  
+
   output$scatterPlot <- renderPlot({
     if (length(input$colMulti) > 1) {
       logDirection <- ""
-      
-      
+
+
       if (input$logx) {
         logDirection <- paste(logDirection, "x", sep = "")
       }
-      
+
       if (input$logy) {
         logDirection <- paste(logDirection, "y", sep = "")
       }
-      
+
       formula <-
         as.formula(paste("~ ", paste(
           Map(surroundWhitespace, input$colMulti), collapse = " + "
@@ -172,7 +173,7 @@ showPlots <- function(input, output, dataset) {
       )
     }
   })
-  
+
   output$correlationPlot <- renderPlot({
     if (dim(dataset())[2] > 1) {
       pairs(dataset(),
@@ -180,7 +181,7 @@ showPlots <- function(input, output, dataset) {
             upper.panel = panel.cor)
     }
   })
-  
+
   output$selfChoosyCorrelationPlot <- renderPlot({
     if (length(input$using) > 1) {
       formula <-
@@ -195,29 +196,29 @@ showPlots <- function(input, output, dataset) {
       )
     }
   })
-  
+
   output$fitting <- renderPlot({
     if (length(input$using) >= 1) {
       lm1 <- makeModel(input, dataset)
       plot(lm1, which = c(1))
     }
   })
-  
+
   output$modelQuantileQuantilePlot <- renderPlot({
     if (length(input$using) >= 1) {
       lm1 <- makeModel(input, dataset)
       plot(lm1, which = c(2))
     }
   })
-  
+
   output$outliers <- renderPlot({
     if (length(input$using) >= 1) {
       lm1 <- makeModel(input, dataset)
       plot(lm1)
     }
   })
-  
-  
+
+
 }
 
 summaries <- function(input, output, dataset) {
@@ -227,7 +228,7 @@ summaries <- function(input, output, dataset) {
       summary(lm1)
     }
   })
-  
+
 }
 
 server <- function(input, output) {
@@ -244,35 +245,35 @@ server <- function(input, output) {
       }
     )
   })
-  
+
   output$binsSlider<- renderUI({
     sliderInput("bins", "Histogram bins:" , min = 1, max = length(dataset()[[input$col]]), value = length(dataset()[[input$col]]) / 4)
   })
-  
+
   output$datasetColumns <- renderUI({
     selectInput("col", "Variable:", colnames(dataset()))
   })
-  
+
   output$datasetColumnsMulti <- renderUI({
     selectInput("colMulti", "Variable:", colnames(dataset()), multiple = TRUE)
   })
-  
+
   #It is impossible to use the same output twice https://github.com/rstudio/shiny/issues/743
   output$datasetColumnsMulti2 <- renderUI({
     selectInput("using", "Using:", colnames(dataset()), multiple = TRUE)
   })
-  
+
   output$datasetColumnsExplain <- renderUI({
     selectInput("explain", "Explain:", colnames(dataset()))
   })
-  
+
   location(input, output, dataset)
-  
+
   variation(input, output, dataset)
-  
+
   distribution(input, output, dataset)
-  
+
   showPlots(input, output, dataset)
-  
+
   summaries(input, output, dataset)
 }
